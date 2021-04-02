2c76a898b267bb8b288b7c1d09a299db5a164f5d
Jakelynn Walker <jakelynnwalker15@icloud.com>
2021-04-02 11:56:46 -0500

    operator

---
+++ Operator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            string result = $"{a} / {b}";
+
+            Console.WriteLine("result");
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
